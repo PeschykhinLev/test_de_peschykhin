ff0324a70884f5cb75e008285ffcdcb3f6e5a07e
Lev <lev.peschykhin@mantisanalytics.com>
2024-10-12 13:28:36 +0300

    Updating plugins

--- plugins/data_generator/generator.py
+++ plugins/data_generator/generator.py
@@ -5,38 +5,6 @@ from random import uniform, choice
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
 
-# def generate_insert_order_data(batch_size: int) -> None:
-#     currencies = ['USD', 'EUR', 'GBP', 'CAD', 'AUD', 'JPY', 'CHF', 'CNY', 'HKD', 'NZD']
-    
-#     data = {
-#         'order_id': [],
-#         'customer_email': [],
-#         'order_date': [],
-#         'amount': [],
-#         'currency': []
-#     }
-
-#     try:
-#         for _ in range(batch_size):
-#             order_id = str(uuid.uuid4())
-#             data['order_id'].append(order_id)
-#             data['customer_email'].append(f"customer{order_id[:5]}@gmail.com")
-#             order_date = datetime.now() - timedelta(days=uniform(0, 7))
-#             data['order_date'].append(order_date.strftime('%Y-%m-%d %H:%M:%S'))
-#             data['amount'].append(round(uniform(1, 1000), 2))
-#             data['currency'].append(choice(currencies))
-
-#         df = pd.DataFrame(data)
-#         pg_hook = PostgresHook(postgres_conn_id='postgres_1')
-
-#         engine = pg_hook.get_sqlalchemy_engine()        
-#         df.to_sql('orders', con=engine, if_exists='append', index=False, method='multi', chunksize=1000)
-#         logging.info(f"Successfully inserted {batch_size} records into orders table")
-
-#     except Exception as e:
-#         logging.error(f"Error in generate_insert_order_data: {str(e)}")
-#         raise AirflowException(f"Failed to generate and insert order data: {str(e)}")
-
 class GenerateInsertOrderDataOperator(BaseOperator):
 
     def __init__(self, batch_size, postgres_conn_id = 'postgres_1', *args, **kwargs):
